eda43fef9caca18dc71be2f11f98b6b1dad289f3
Benjamin Soung <soungbenjamin@gmail.com>
2017-03-09 18:18:48 -0500

    Implemented fetch to get all events from eventable

--- src/app/App.js
+++ src/app/App.js
@@ -21,10 +21,50 @@ class App extends Component {
     }
 
     console.log('found token!')
+    // this.fetchEvents('https://api.eventable.com/v1/events/')
     // TODO fetchEvents
   }
 
+  fetchEvents(url) {
+    const options = {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "token " + localStorage.getItem('token')
+      }
+    }
 
+    fetch(url, options)
+      .then(res => {
+        return res.json()
+      })
+      .then(res => {
+        console.log(res.results)
+
+        const events = res.results;
+
+        for (let i = 0, len = events.length; i < len; i++) {
+          this.addEvent(events[i]);
+        }
+        
+        return res;
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  addEvent = (title, startTime, endTime) => {
+    let events = [...this.state.events];
+
+    let newEvent = {title, startTime, endTime};
+
+    events.push(newEvent);
+
+    this.setState({
+      events,
+      filteredEvents: events
+    })
+  }
 
   render() {
     return (
--- src/utils/APIManager.js
+++ src/utils/APIManager.js
@@ -1,6 +1,34 @@
 import 'whatwg-fetch';
+import _ from 'lodash';
 
 export default {
+	get: (url, cb) => {
+		const options = {
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": "token " + localStorage.getItem('token')
+			}
+		}
+
+		fetch(url, options)
+			.then(res => {
+				return res.json()
+			})
+			.then(res => {
+				console.log(res.results)
+
+				if (_.isFunction(cb)) {
+					cb();
+				}
+				
+				return res;
+			})
+			.catch(err => {
+				console.error(err)
+			})
+	},
+
+
 	post: (url) => {
 		const data = {
       username: 'candidate.5545@eventable.com',
